c7a94ca5981bb96c7827a351a00efc0a4f9ce3c7
jkrk678 <74199656+jkrk678@users.noreply.github.com>
2022-05-16 08:08:32 -0500

    Updated db host

--- server.js
+++ server.js
@@ -14,11 +14,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    port : 5432,
-    user : 'postgres',
-    password : 'psql',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    }
   }
 });
 
